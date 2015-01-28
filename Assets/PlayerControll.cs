1d6923e194959cb1e602f7c60e39194d6e814d7d
Perkovec <perkovec@icloud.com>
2015-01-28 20:28:57 +0300

    Add Project

---
+++ Assets/PlayerControll.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControll : MonoBehaviour {
+
+	public Transform LeftJoy;
+	public Transform RightJoy;
+	public Transform Center;
+	public Transform Stick;
+	CNJoystick CNLeft;
+	CNJoystick CNRight;
+	Vector3 movement;
+	// Use this for initialization
+	void Start () {
+		CNLeft = LeftJoy.GetComponent<CNJoystick> ();
+		CNRight = RightJoy.GetComponent<CNJoystick> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+
+		Debug.Log ("Horizontal: " + CNLeft.GetAxis ("Horizontal") + " Vertical: " + CNLeft.GetAxis ("Vertical"));
+
+	}
+
+
+}
